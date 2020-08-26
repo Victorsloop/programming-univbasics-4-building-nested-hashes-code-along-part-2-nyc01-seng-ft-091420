6b9595af054ef8029e5135567d6ee66510f419c2
Victor Torres <torres.victor136@gmail.com>
2020-08-26 03:19:08 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -24,7 +24,7 @@ def update_status
   # Code your solution here:
 epic_tragedy[:montague][:hero][:status] = "dead"
 
-
+epic_tragedy[:capulet][:heroine][:status] = "dead"
 
 
   # After your code, the following code will return the altered hash. This method must only return the modified epic_tragedy hash
@@ -61,7 +61,7 @@ def add_characters
    #   - The second hash should have the :name key set to "Apothecary" 
    # Code your solution here:
 
-
+epic_tragedy[ :additional_characters ] = { name: "Prince Escalus"}, { name : "Apothecary"
 
 
 
